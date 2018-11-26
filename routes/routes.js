96ff0b639b49a133cb55e4edf4ea47b04cc18318
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-26 11:19:43 -0500

    change routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 const pool = require("../connection/connection.js");
 
 function getTable(req, res) {
-    pool.query("SELECT * FROM shoppingcart ORDER BY id").then((result) => {
+    pool.query("SELECT * FROM shopping_cart ORDER BY id").then((result) => {
         res.json(result.rows);
     });
 }
@@ -14,19 +14,19 @@ routes.get("/products", (req, res) => {
 });
 
 routes.post("/products", (req, res) => {
-    pool.query("INSERT INTO shoppingcart(product, price, quantity) VALUES($1::text, $2::int, $3::int)", [req.body.product, req.body.price, req.body.quantity]).then(() => {
+    pool.query("INSERT INTO shopping_cart(product, price, quantity) VALUES($1::text, $2::int, $3::int)", [req.body.product, req.body.price, req.body.quantity]).then(() => {
         getTable(req, res);
     });
 });
 
 routes.put("/products/:id", (req, res) => {
-    pool.query("UPDATE shoppingcart SET product=$1::text, price=$2::int, quantity=$3::int WHERE id=$4::int", [req.body.product, req.body.price, req.body.quantity, req.params.id]).then(() => {
+    pool.query("UPDATE shopping_cart SET product=$1::text, price=$2::int, quantity=$3::int WHERE id=$4::int", [req.body.product, req.body.price, req.body.quantity, req.params.id]).then(() => {
         getTable(req, res);
     });
 });
 
 routes.delete("/products/:id", (req, res) => {
-    pool.query("DELETE FROM shoppingcart WHERE id=$1::int", [req.params.id]).then(() => {
+    pool.query("DELETE FROM shopping_cart WHERE id=$1::int", [req.params.id]).then(() => {
         getTable(req, res);
     });
 });
