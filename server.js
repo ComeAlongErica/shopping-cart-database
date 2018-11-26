7a8b2f2bffa497e3297bcc2b211833528aba0907
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-26 10:34:25 -0500

    commit for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 const routes = require("./routes/routes.js");
 
 app.use(express.static("./public"));
